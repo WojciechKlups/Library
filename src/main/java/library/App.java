6b2eb0851147ef2833741ed9cb0d0ad9b30e7e4e
Wojciech Klupś <wojciech.klups@gmail.com>
2019-04-27 14:37:02 +0200

    create library

---
+++ src/main/java/library/App.java
@@ -0,0 +1,7 @@
+package library;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/library/HibernateUtils.java
@@ -0,0 +1,22 @@
+package library;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+
+    private final static SessionFactory sf = new Configuration()
+            .configure()
+            .buildSessionFactory();
+
+    private static Session session = sf.openSession();
+
+    public static Session getHibernateSession() {
+        if (session == null) {
+            session = (Session) new HibernateUtils();
+        }
+        return session;
+    }
+}
+
